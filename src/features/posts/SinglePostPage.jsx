6c7aa4f2206bfba94f375dcb6a24b21e3da3b504
Karel Ježík <jezikkarel@gmail.com>
2023-01-17 20:12:46 +0100

    added chaining op

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -6,43 +6,46 @@ import { selectAllUsers } from "../users/userSlice";
 
 import { useParams, Link } from "react-router-dom";
 
-
 const SinglePostPage = () => {
   // retrieve postId - we are gonna get it from useParam - :postid url parameter
 
-  const { postId } = useParams() // its gonna pull postId from URL parameter
+  const { postId } = useParams(); // its gonna pull postId from URL parameter
 
   const users = useSelector(selectAllUsers);
-  const post = useSelector(state => selectPostById(state, postId)) // have to have callback to post both state, and postId
+  const post = useSelector((state) => selectPostById(state, postId)); // have to have callback to post both state, and postId
 
-  
   if (!post) {
     return (
       <section>
         <h2>Post not found!</h2>
-        <p>State refreshes to default fake API values, no new posts can be showed after browser refresh </p>
+        <p>
+          State refreshes to default fake API values, no new posts can be showed after
+          browser refresh{" "}
+        </p>
       </section>
-    )
+    );
   }
 
-  const user = users.find((user) => user.id == post?.userId);  //post is too slow, post.userId throws undefined sometimes
+  const user = users.find((user) => user.id == post?.userId); //post is too slow, post.userId throws undefined sometimes
 
-  return (<section>
-    <article>
-      <p className="parInPost">
-        By: <i>{user.name}</i>
-      </p>
-      <h3>{post.title}</h3>
-      <h4>{post.body}</h4>
-      <p className="parInPost">
-        <Link to={`/post/edit/${post.id}`}> Edit Post </Link>
-        <i>
-          <TimeAgo timestamp={post.date} />
-        </i>
-      </p>
-      <PostReactionButtons post={post} />
-    </article></section>
-  )
+  return (
+    <section>
+      <article>
+        <p className="parInPost">
+          By: <i>{user?.name}</i>
+        </p>
+        <h3>{post.title}</h3>
+        <h4>{post.body}</h4>
+        <p className="parInPost">
+          <Link to={`/post/edit/${post.id}`}> Edit Post </Link>
+          <i>
+            <TimeAgo timestamp={post.date} />
+          </i>
+        </p>
+        <PostReactionButtons post={post} />
+      </article>
+    </section>
+  );
 };
 
 export default SinglePostPage;
